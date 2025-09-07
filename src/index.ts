8b7f14cbc7cac75f9c2379a688bd215322b86b0d
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-09-07 21:27:50 +0545

    user routes defined and connected with index file

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,8 @@ app.use(express.json());
 
 //Routes
 app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
