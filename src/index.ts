af84d03b46be030c1510ffa9710c6ed16f6f7325
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-09-07 21:08:12 +0545

    some codes added to index file

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express  from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
