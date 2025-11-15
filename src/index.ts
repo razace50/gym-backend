5a47096facfd6b73cd6bc8e3e04e0568acee28ab
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-11-15 21:40:17 +0545

    some updates

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 //Routes
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
