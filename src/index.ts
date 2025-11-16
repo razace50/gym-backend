8ac36c9130f8f37d6cd970a48ac309b44483de26
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-11-16 22:25:19 +0545

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import express  from 'express';
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes";
-
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const port = process.env.PORT || 5001;
+
+//Middlewares
 app.use(express.json());
+app.use(cors);
 
 //Routes
-app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5001
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+//Error handling middleware
+
+//Server running
+app.listen(port, () => {
+    console.log(`Server is running on localhost:${port}`);
+})
